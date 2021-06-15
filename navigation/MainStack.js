4ca527811ff8106845fd4d43d9951b252b664b3a
Jeetesh Kumar Shaw <jeeteshshaw@gmail.com>
2021-06-15 22:56:33 +0530

    Course data display done

--- navigation/MainStack.js
+++ navigation/MainStack.js
@@ -7,6 +7,8 @@ import RText from '../components/common/RText'
 import FilterCategory from '../screen/MainScreen/FilterCategory'
 import CourseInfo from '../screen/MainScreen/CourseInfo'
 import CourseHome from '../screen/MainScreen/CourseHome'
+import CourseData from '../screen/MainScreen/CourseData'
+import { Text,View,Image,TouchableOpacity } from 'react-native'
 
 
 const Stack = createStackNavigator()
@@ -24,7 +26,39 @@ function MainStack() {
                
            }}/>
            <Stack.Screen name='CourseInfo' component={CourseInfo} />
-           <Stack.Screen name='CourseHome' component={CourseHome} />
+           <Stack.Screen 
+                name='CourseHome' 
+                component={CourseHome} 
+                options={({route, navigation})=>({
+                    title:"",
+                    headerLeft:()=> (
+                        <View style={{flexDirection:'row', justifyContent:'space-between', marginLeft:8}}>
+                            <TouchableOpacity onPress={()=>navigation.goBack()} style={{justifyContent:'center'}}>
+                                <Image source={require('../assets/icons/arrow-back.png')} style={{width:27, height:27}} />
+                            </TouchableOpacity>
+                            <View style={{marginHorizontal:16}}>
+                                <RText style={{color:'#1A1A1A', fontFamily:'Roboto-Bold', fontSize:17}}>Design for Human Mind {'&'} AI</RText>
+                                <RText style={{color:'#1A1A1A', fontFamily:'Roboto-Light-Italic', fontSize:12}}>by Riverside Design Studio</RText>
+                            </View>
+                        </View>
+                    ),
+                    headerRight:()=>(
+                        
+                        <View style={{borderRadius:30, backgroundColor:'#FFB103', marginHorizontal:16}}>
+                            <Image source={require('../assets/icons/trophy.png')} style={{width:20, height:20, margin:8}} />
+                        </View>
+                    )  
+                })}
+
+           />
+           <Stack.Screen 
+                name='CourseData' 
+                component={CourseData} 
+                options={({route})=>({
+                    title: route.params.headerTitle
+                })}
+            />
+           
        </Stack.Navigator>
     )
 }
--- screen/MainScreen/CourseHome.js
+++ screen/MainScreen/CourseHome.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import { Dimensions, StyleSheet, View, Button, TouchableOpacity, Image, FlatList } from 'react-native'
 import RText from '../../components/common/RText'
 import RootComponent from '../RootComponent'
-import { Video, AVPlaybackStatus } from 'expo-av';
 import AText from '../../components/common/AText';
+import VideoPlayer from '../../components/custom/VideoPlayer';
 
 const WIDTH = Dimensions.get('window').width;
 const HEIGHT = Dimensions.get('window').height;
@@ -54,12 +54,12 @@ const WeeklyData= [
 ]
 
 
-function CourseHome() {
+function CourseHome(props) {
     const [selectedOption, setSelectedOption] = useState(0)
     return (
         <View style={{flex:1, backgroundColor:'#C4C4C4'}}>
             <View style={{width:WIDTH, height:220, backgroundColor:'#000'}}>
-                <VideoPlayer />
+                <VideoPlayer video={require('../../assets/videos/Need-for-Speed-Heat-Official.mp4')} />
             </View>
             <View style={{padding:16}}>
                 <FlatList 
@@ -79,7 +79,9 @@ function CourseHome() {
                     data={WeeklyData}
                     ItemSeparatorComponent={()=>(<View style={{height:1, backgroundColor:'#B2B2B2'}}></View>)}
                     renderItem={({item, index})=>(
-                        <View style={{flexDirection:'row', alignItems:'center', marginVertical:16}}>
+                        <TouchableOpacity 
+                        onPress={()=> props.navigation.navigate('CourseData', {headerTitle: item.title})}
+                        style={{flexDirection:'row', alignItems:'center', marginVertical:16}}>
                             <AText style={{marginHorizontal:16,fontFamily: 'Arial-Bold'}}>{index+1}.</AText>
                             <View style={{marginHorizontal:8, backgroundColor:'#fff', borderRadius:30, overflow:'hidden', alignItems:'center'}}>
                                 <Image 
@@ -88,7 +90,7 @@ function CourseHome() {
                                     />        
                             </View>
                             <RText style={{fontFamily: 'Roboto', marginHorizontal: 16}}>{item.title}</RText>
-                        </View>
+                        </TouchableOpacity>
                     )}
                     keyExtractor={(item, index)=>"fsdfafafa"+index+item.title}
                 />
@@ -101,40 +103,40 @@ export default RootComponent(CourseHome, false)
 
 
 
-const VideoPlayer = (props) => {
-    const video = React.useRef(null);
-    const [status, setStatus] = React.useState({});
-    const [PlayedOnce, setPlayedOnce] = useState(false)
-    return (
-        <View style={styles.container}>
-            <Video
-                ref={video}
-                style={styles.video}
-                source={require('../../assets/videos/Need-for-Speed-Heat-Official.mp4')}
-                useNativeControls
-                resizeMode="contain"
-                isLooping
-                onPlaybackStatusUpdate={status => setStatus(() => status)}
-            />
-            {
-                PlayedOnce
-                    ? null
-                    : (
+// const VideoPlayer = (props) => {
+//     const video = React.useRef(null);
+//     const [status, setStatus] = React.useState({});
+//     const [PlayedOnce, setPlayedOnce] = useState(false)
+//     return (
+//         <View style={styles.container}>
+//             <Video
+//                 ref={video}
+//                 style={styles.video}
+//                 source={require('../../assets/videos/Need-for-Speed-Heat-Official.mp4')}
+//                 useNativeControls
+//                 resizeMode="contain"
+//                 isLooping
+//                 onPlaybackStatusUpdate={status => setStatus(() => status)}
+//             />
+//             {
+//                 PlayedOnce
+//                     ? null
+//                     : (
 
-                        <TouchableOpacity
-                            style={styles.buttons}
-                            onPress={() => {
-                                video.current.playAsync()
-                                setPlayedOnce(true)
-                            }}>
-                            <Image source={require('../../assets/icons/Polygon1.png')} style={{ width: 28, height: 32, tintColor: '#fff' }} />
-                        </TouchableOpacity>
-                    )
-            }
-        </View>
+//                         <TouchableOpacity
+//                             style={styles.buttons}
+//                             onPress={() => {
+//                                 video.current.playAsync()
+//                                 setPlayedOnce(true)
+//                             }}>
+//                             <Image source={require('../../assets/icons/Polygon1.png')} style={{ width: 28, height: 32, tintColor: '#fff' }} />
+//                         </TouchableOpacity>
+//                     )
+//             }
+//         </View>
 
-    )
-}
+//     )
+// }
 
 
 const styles = StyleSheet.create({
