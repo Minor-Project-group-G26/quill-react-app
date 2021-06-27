09ae817aacfcb860610ee597165bf44590a0764c
Jeetesh Kumar Shaw <jeeteshshaw@gmail.com>
2021-06-27 18:51:53 +0530

    Before Merge Gourav

--- store/index.js
+++ store/index.js
@@ -2,6 +2,7 @@ import {configureStore} from '@reduxjs/toolkit' ;
 import AuthSlice from './AuthSlice';
 import RouterSlice from './RouterSlice';
 import SwapStoreSlice from './SwapStoreSlice';
+import UserSlice from './UserSlice';
 // import {} from '@reduxjs/toolkit'
 
 
@@ -9,6 +10,7 @@ const store = configureStore({
     reducer:{
         auth: AuthSlice,
         swapStore: SwapStoreSlice,
+        user: UserSlice
         // routerController: RouterSlice
     }
 })
